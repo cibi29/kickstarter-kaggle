42ae622b8cd256386f96735db7725f9841003879
thearnavgarg <thearnavgarg@gmail.com>
2018-11-14 12:31:47 -0800

    Count Plot Chart

--- data_analysis.py
+++ data_analysis.py
@@ -6,28 +6,31 @@ import seaborn as sns
 
 
 pd.set_option('display.max_columns', None)
+pd.options.display.float_format = '{:.0f}'.format
 
 
 def first_pass_feature_filter(data):
-    Y = data.state
+    drop_list = ['suspended', 'live', 'undefined']
+    data = data[~data.isin(drop_list)]
+    y = data.state
     drop_list = ['name', 'state', 'ID']
     X = data.drop(drop_list, axis=1)
-    return X, Y
+    return X, y
 
 
-def count_plot(Y):
-    sns.countplot(Y, label="Count")
+def count_plot(y):
+    sns.countplot(y, label="Count")
     plt.show()
-
 
 def main():
     csv_filepath = os.path.join('kickstarter-projects', 'ks-projects-201801.csv')
     data = pd.read_csv(csv_filepath)
+
     # Dividing the data into two sets (domain and label) and removing
     # the obvious ones that we don't need.
-    X, Y = first_pass_feature_filter(data)
-
-    count_plot(Y)
+    X, y = first_pass_feature_filter(data)
+    # remove_irrelevant_data(X, y)
+    count_plot(y)
 
 
 if __name__ == '__main__':
